9fc1ed865eec9f1228c208b9c10b71c1c9d0ccdb
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-06-09 16:35:53 -0700

    all functions work

--- src/channel.py
+++ src/channel.py
@@ -6,10 +6,23 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
-
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+
+        channel = Channel.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = channel['items'][0]['snippet']['thumbnails']["default"]['url']
+        self.subscriberCount = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.viewCount = channel['items'][0]['statistics']['viewCount']
+
+    def __repr__(self):
+        return self.title, self.description, self.url, self.subscriberCount, self.video_count, self.viewCount
+
 
 
     def print_info(self) -> None:
@@ -18,3 +31,19 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+    def to_json(self, filename):
+        self.filename = filename
+
+        channel_info = {"title": self.title, "description": self.description,
+                        "video_count": self.video_count, "url": self.url,
+                        "subscriberCount": self.subscriberCount, "viewCount": self.viewCount}
+
+        with open("../homework-2/moscowpython.json", "w") as file:
+            json.dump(channel_info, file)
+
